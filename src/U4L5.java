06f85cc489b6b08d5e7b2e406947378becbb7430
unknown <BT_4N2_21@BKTECH.LOCAL>
2022-11-03 12:34:08 -0400

    Initial commit

---
+++ src/U4L5.java
@@ -0,0 +1,9 @@
+public class U4L5 {
+    public static void main(String[] args) {
+        StringLoops loops = new StringLoops();
+        System.out.println(loops.reverseString2("hello!"));
+        System.out.println(loops.reverseString2("Apples and bananas"));
+
+
+    }
+}
